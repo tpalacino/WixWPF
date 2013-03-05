93f00df157739a3daea2c24aa77bc1f121b6bd67
tpalacino_cp <SND\tpalacino_cp>
2013-03-05 01:50:21 +0000

    Branched to v3.7

---
+++ v3.7/Example/ProductBA/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProductBA")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProductBA")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2e4f6fa2-b200-45aa-b8fe-a0a5bc3d6400")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ v3.7/WixWPF/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using WixWPF;
+
+[assembly: AssemblyTitle("WixWPF")]
+[assembly: AssemblyDescription("Provides a managed bootstrapper application that allows completely custom user interface with minimal requirements.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Troy Palacino")]
+[assembly: AssemblyProduct("WixWPF")]
+[assembly: AssemblyCopyright("Copyright (c) 2013 Troy Palacino")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5abb66a2-3ff9-4009-9df5-81af340d964c")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: BootstrapperApplication(typeof(WPFBootstrapper))]
