888c4af690b6a0e7898d3c5fb8c6d8230e55c27e
tpalacino_cp <SND\tpalacino_cp>
2013-03-08 04:12:56 +0000

    Merged installer and updates forward from the 3.6 version into 3.7

---
+++ v3.7/Templates/WPFBootstrapperSolution/BootstrapperUI/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using WixWPF;
+using Wix = Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+
+namespace $safeprojectname$
+{
+	/// <summary>Interaction logic for MainWindow.xaml</summary>
+	public partial class MainWindow : BaseBAWindow
+	{
+		#region Constructors
+
+		/// <summary>Creates a new instance of <see cref="MainWindow" />.</summary>
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		#endregion Constructors
+	}
+}
