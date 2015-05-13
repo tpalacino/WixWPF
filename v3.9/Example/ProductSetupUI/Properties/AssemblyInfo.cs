6fd231690f3bc0eae5a1b0d6f4af3722e21b3c18
tpalacino_cp <SND\tpalacino_cp>
2015-05-13 04:53:09 +0000

    Initial check in of the v3.9 branch The WixWPF v3.9 assembly has been implemented

---
+++ v3.9/Example/ProductSetupUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using ProductSetupUI;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using WixWPF;
+
+[assembly: AssemblyTitle("ProductSetupUI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProductSetupUI")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2e4f6fa2-b200-45aa-b8fe-a0a5bc3d6400")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: StartupWindow(typeof(MainWindow))]
---
+++ v3.9/WixWPF39/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using WixWPF;
+
+[assembly: Guid("77a28ac9-8e9d-430b-b691-fd7df713fbd8")]
+
+[assembly: AssemblyVersion("3.9.0.0")]
+[assembly: AssemblyFileVersion("3.9.0.0")]
+
+[assembly: BootstrapperApplication(typeof(WPFBootstrapper))]
