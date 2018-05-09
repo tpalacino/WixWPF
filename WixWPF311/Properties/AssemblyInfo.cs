bf4c440eeaf6945a1c6c6d52b5dc9d7ce2077daf
igece <igece@igecelabs.net>
2018-05-10 01:29:41 +0200

    Version 3.11 Support for WiXToolset 3.11 and VS 2017

---
+++ WixWPF311/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using WixWPF;
+
+[assembly: Guid("8e09008a-ada5-4f17-8243-bbfd52424a49")]
+
+[assembly: AssemblyVersion("3.11.0.0")]
+[assembly: AssemblyFileVersion("3.11.0.0")]
+
+[assembly: BootstrapperApplication(typeof(WPFBootstrapper))]
+
